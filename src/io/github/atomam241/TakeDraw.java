5e5a5aed1d4fcffa4e7788ee8479f5cd693f6142
brockstar17 <brockdyer.git@gmail.com>
2017-01-14 12:20:36 -0500

    Fixed AI name display

--- src/io/github/atomam241/TakeDraw.java
+++ src/io/github/atomam241/TakeDraw.java
@@ -25,7 +25,7 @@ public class TakeDraw extends JPanel {
 		g.drawRect(290, 80, 60, 400);
 
 		g.drawString("Player 1", 20, 70);
-		g.drawString(pl2, 300, 70);
+		g.drawString(pl2, 290, 70);
 
 		int y = 1, x = 1;
 
